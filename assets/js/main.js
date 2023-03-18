13ef75d64fd2725de5b865f6248a98abf7dd53cc
biaccshop <biaccshopinfo@gmail.com>
2023-03-18 06:30:21 +0300

    New Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,8 +41,21 @@ ScrollReveal().reveal('.about-content, .services-title, .portfolio-title, .faq-t
 ScrollReveal().reveal('.about-img', {origin: 'right'});
 
 // Console bug
-Object.defineProperty(window, "console", {
-    value: console,
-    writable: false,
-    configurable: false
-  });
+// Disable right-click
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
