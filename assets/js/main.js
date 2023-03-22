b8d4e1dbc34ad9cb75df570f0c68bac2a1ec6ba0
biaccshop <biaccshopinfo@gmail.com>
2023-03-22 11:34:51 +0300

    New Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,17 +85,17 @@ ScrollReveal().reveal('.question', { origin: 'bottom', delay: 700 });
 // Disable right-click
 document.addEventListener('contextmenu', (e) => e.preventDefault());
 function ctrlShiftKey(e, keyCode) {
-  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+    return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
 }
 
 // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
 document.onkeydown = (e) => {
-  if (
-    event.keyCode === 123 ||
-    ctrlShiftKey(e, 'I') ||
-    ctrlShiftKey(e, 'J') ||
-    ctrlShiftKey(e, 'C') ||
-    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-  )
-    return false;
+    if (
+        event.keyCode === 123 ||
+        ctrlShiftKey(e, 'I') ||
+        ctrlShiftKey(e, 'J') ||
+        ctrlShiftKey(e, 'C') ||
+        (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+    )
+        return false;
 };
