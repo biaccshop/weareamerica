dd683d21c36ee84ea2ae087baa01905dc3c2c53e
biaccshop <biaccshopinfo@gmail.com>
2023-05-07 09:52:18 +0300

    New Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,3 @@
-// URL Redirecting
-  addEventListener("fetch", event => {
-    event.respondWith(handleRequest(event.request))
-  })
-
-  /**
-   * Respond to the request
-   * @param {Request} request
-   */
-  async function handleRequest(request) {
-    const url = new URL(request.url)
-    if (url.pathname === '/tos') {
-      return fetch('https://biacc.shop/tos.html')
-    }
-    else if (url.pathname === '/home') {
-      return fetch('https://biacc.shop/index.html')
-    }
-    
-    return fetch(request)
-  }
-
-
 // Navbar anchor + hiding # in URL
 $('a[href^="#"]').on('click', function (event) {
   var target = $(this.getAttribute('href'));
