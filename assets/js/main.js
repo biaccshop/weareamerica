7335af2a42135359de04f6773b701ae237b048d0
biaccshop <biaccshopinfo@gmail.com>
2023-07-28 14:59:51 +0300

    New Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,57 @@
+// Disable right-click
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+function ctrlShiftKey(e, keyCode) {
+    return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+document.onkeydown = (e) => {
+    if (
+        event.keyCode === 123 ||
+        ctrlShiftKey(e, 'I') ||
+        ctrlShiftKey(e, 'J') ||
+        ctrlShiftKey(e, 'C') ||
+        (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+    )
+        return false;
+};
+
+
+
+// Scroll Reveal
+ScrollReveal({
+  reset: false,
+  distance: '200px',
+  duration: 1500,
+  delay: 200
+});
+
+ScrollReveal().reveal('#announcement', { origin: 'top', distance: '50px', delay: 200 });
+
+ScrollReveal().reveal('.header', { origin: 'top', distance: '0px', delay: 200, duration: 2000 });
+
+ScrollReveal().reveal('.hero-content', { origin: 'bottom', delay: 200 });
+ScrollReveal().reveal('.hero-img', { origin: 'top', delay: 200 });
+
+ScrollReveal().reveal('.about-content h2, .about-content h3', { origin: 'right', delay: 200 });
+ScrollReveal().reveal('.about-img', { origin: 'left', delay: 200 });
+ScrollReveal().reveal('.about-content .d1', { origin: 'bottom', delay: 700 });
+ScrollReveal().reveal('.about-content .d2', { origin: 'bottom', delay: 800 });
+ScrollReveal().reveal('.about-content .d3', { origin: 'bottom', delay: 900 });
+
+ScrollReveal().reveal('.as-seen-on', { origin: 'bottom', distance: '0px', delay: 700 });
+
+ScrollReveal().reveal('.statistics', { origin: 'bottom', distance: '50px', delay: 500 });
+
+ScrollReveal().reveal('.services .services-title', { origin: 'bottom', distance: '50px', delay: 200 });
+ScrollReveal().reveal('.services .services-content', { origin: 'bottom', distance: '50px', delay: 500 });
+
+ScrollReveal().reveal('.feedbacks-title', { origin: 'bottom', distance: '50px', delay: 200 });
+ScrollReveal().reveal('.feedbacks .slider', { origin: 'bottom', distance: '0px', delay: 500 });
+
+ScrollReveal().reveal('.contact .card', { origin: 'bottom', distance: '50px', delay: 500 });
+
+
+
 // Navbar anchor + hiding # in URL
 $('a[href^="#"]').on('click', function (event) {
   var target = $(this.getAttribute('href'));
@@ -34,122 +88,49 @@ $(document).ready(function () {
 
 
 
-// FAQ section
-const faqQuestions = document.querySelectorAll(".faq-container .question");
-
-faqQuestions.forEach((faqQuestion) => {
-  faqQuestion.addEventListener("click", () => {
-    faqQuestions.forEach((otherQuestion) => {
-      if (otherQuestion !== faqQuestion && otherQuestion.classList.contains("active")) {
-        const otherAnswer = otherQuestion.nextElementSibling;
-        otherAnswer.classList.remove("active");
-        otherQuestion.classList.remove("active");
-      }
-    });
-
-    const answer = faqQuestion.nextElementSibling;
-    answer.classList.toggle("active");
-    faqQuestion.classList.toggle("active");
+// Modal section
+$(document).ready(function () {
+  var modal = $(".modal");
+  // Function to add a flag value to localStorage when the modal is closed.
+  function setModalClosed() {
+    localStorage.setItem("modalClosed", "true");
+  }
+  // Check the flag value in localStorage when the page loads to display the modal if needed.
+  var modalClosedFlag = localStorage.getItem("modalClosed");
+  if (modalClosedFlag === null) {
+    modal.css("display", "block");
+  }
+  modal.on("click", ".close", function () {
+    modal.css("display", "none");
+    setModalClosed(); // Add the flag value to localStorage when the modal is closed.
   });
-});
-
-
-
-// Category Buttons Listeners
-document.querySelectorAll('.btn-category').forEach(function (button) {
-  button.addEventListener('click', function () {
-    // Remove active class from all buttons
-    document.querySelectorAll('.btn-category').forEach(function (button) {
-      button.classList.remove('active');
-    });
-    // Add active class to clicked button
-    this.classList.add('active');
-
-    var category = this.getAttribute('data-category');
-    var portfolio = document.querySelectorAll('.portfolio-box');
-    if (category == 'all') {
-      portfolio.forEach(function (product) {
-        product.style.display = 'flex';
-        product.style.opacity = '0';
-        setTimeout(function () {
-          product.style.transition = 'opacity 0.3s, transform 0.3s, box-shadow 0.3s';
-          product.style.opacity = '1';
-        }, 0);
-      });
-    } else {
-      portfolio.forEach(function (product) {
-        if (product.getAttribute('data-category') == category) {
-          product.style.display = 'flex';
-          product.style.opacity = '0';
-          setTimeout(function () {
-            product.style.transition = 'opacity 0.3s, transform 0.3s, box-shadow 0.3s';
-            product.style.opacity = '1';
-          }, 0);
-        } else {
-          product.style.display = 'none';
-        }
-      });
+  modal.on("click", "a", function () {
+    modal.css("display", "none");
+    setModalClosed(); // Add the flag value to localStorage when the modal is closed.
+  });
+  $(document).on("click", function (event) {
+    if ($(event.target).is(modal)) {
+      modal.css("display", "none");
+      setModalClosed(); // Add the flag value to localStorage when the modal is closed.
     }
   });
 });
 
 
 
-// Scroll Reveal
-ScrollReveal({
-  reset: false,
-  distance: '200px',
-  duration: 1500,
-  delay: 200
+// Announcement
+const announcementClose = document.querySelector(".announcement-close");
+const announcementSection = document.querySelector("#announcement");
+
+announcementClose.addEventListener("click", () => {
+  announcementSection.classList.add("hidden");
+  sessionStorage.setItem("announcementHidden", "true");
 });
 
-ScrollReveal().reveal('.hero-content', { origin: 'bottom', delay: 200 });
-ScrollReveal().reveal('.hero-img', { origin: 'top', delay: 200 });
-
-ScrollReveal().reveal('.about-content h2, .about-content h3', { origin: 'right', delay: 200 });
-ScrollReveal().reveal('.about-img', { origin: 'left', delay: 200 });
-ScrollReveal().reveal('.about-content .d1', { origin: 'bottom', delay: 700 });
-ScrollReveal().reveal('.about-content .d2', { origin: 'bottom', delay: 800 });
-ScrollReveal().reveal('.about-content .d3', { origin: 'bottom', delay: 900 });
-
-ScrollReveal().reveal('.as-seen-on', { origin: 'bottom', distance: '0px', delay: 700 });
-
-ScrollReveal().reveal('.statistics', { origin: 'bottom', distance: '50px', delay: 500 });
-
-ScrollReveal().reveal('.services .services-title', { origin: 'bottom', distance: '50px', delay: 200 });
-ScrollReveal().reveal('.services .services-content', { origin: 'bottom', distance: '50px', delay: 500 });
-
-ScrollReveal().reveal('.feedbacks-title', { origin: 'bottom', distance: '50px', delay: 200 });
-ScrollReveal().reveal('.feedbacks .slider', { origin: 'bottom', distance: '0px', delay: 500 });
-
-ScrollReveal().reveal('.contact .card', { origin: 'bottom', distance: '50px', delay: 500 });
-
-
-
-ScrollReveal().reveal('.portfolio-title', { origin: 'bottom', distance: '50px', delay: 200 });
-ScrollReveal().reveal('.portfolio .category-filter', { origin: 'bottom', distance: '0px', delay: 500 });
-ScrollReveal().reveal('.portfolio .portfolio-content', { origin: 'bottom', distance: '0px', delay: 900 });
-
-ScrollReveal().reveal('.faq-title', { origin: 'bottom', distance: '50px', delay: 200 });
-ScrollReveal().reveal('.faq-container', { origin: 'bottom', distance: '0px', delay: 500 });
-
-ScrollReveal().reveal('.tos', { origin: 'bottom', distance: '50px', delay: 200 });
-
-
-// Disable right-click
-document.addEventListener('contextmenu', (e) => e.preventDefault());
-function ctrlShiftKey(e, keyCode) {
-    return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-}
-
-// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-document.onkeydown = (e) => {
-    if (
-        event.keyCode === 123 ||
-        ctrlShiftKey(e, 'I') ||
-        ctrlShiftKey(e, 'J') ||
-        ctrlShiftKey(e, 'C') ||
-        (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-    )
-        return false;
-};
+// Checking of refresh
+window.addEventListener("load", () => {
+  const isAnnouncementHidden = sessionStorage.getItem("announcementHidden");
+  if (isAnnouncementHidden === "true") {
+    announcementSection.classList.add("hidden");
+  }
+});
